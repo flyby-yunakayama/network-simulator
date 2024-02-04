6d849c8b75fd3a88bd7f4d8a161e7786f4dc68d2
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-02-04 15:02:36 +0900

    modified:   sec6c/Router.py

--- sec6c/Router.py
+++ sec6c/Router.py
@@ -382,7 +382,8 @@ class Router:
             if destination != self.node_id:
                 next_hop = self.find_initial_hop(destination, previous_nodes, self.node_id)
                 link_to_next_hop = self.get_link_to_neighbor(next_hop) if next_hop else None
-                print(f"From {self.node_id} to {destination}: previous_nodes: {previous_nodes.get(destination)}, next_hop:{next_hop}, link: {link_to_next_hop}")
+                if self.network_event_scheduler.routing_verbose:
+                    print(f"From {self.node_id} to {destination}: previous_nodes: {previous_nodes.get(destination)}, next_hop:{next_hop}, link: {link_to_next_hop}")
                 
                 # 宛先ルータの全インターフェースに対するルートを個別に算出
                 for intf_info in self.topology_database[destination]['link_state_info'].values():
