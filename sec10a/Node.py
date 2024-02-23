f7c71a8dddc53f68e0ceaf5887a0bf2381535476
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-02-23 19:47:21 +0900

    modified:   sec10a/Node.py

--- sec10a/Node.py
+++ sec10a/Node.py
@@ -441,12 +441,12 @@ class Node:
         end_time = start_time + duration
         source_port = self.select_random_port()  # 利用可能なランダムなソースポートを選択
         destination_port = self.select_random_port()  # デスティネーションポートもランダムに選択
+        print(f"Starting UDP traffic to {destination_ip} at {bitrate} bps, start_time={start_time}, duration={duration}, header_size={header_size}, payload_size={payload_size}, burstiness={burstiness}, protocol={protocol}")
 
         def generate_packet():
             if self.network_event_scheduler.current_time < end_time:
                 # send_packetメソッドを使用してパケットを送信
                 data = b'X' * payload_size  # ダミーデータを生成
-                print(f"Generating UDP packet to {destination_ip}, {len(data)} bytes., payload_size={payload_size}")
                 self.send_packet(destination_ip, data, protocol, source_port=source_port, destination_port=destination_port)
 
                 # 次のパケットをスケジュールするためのインターバルを計算
