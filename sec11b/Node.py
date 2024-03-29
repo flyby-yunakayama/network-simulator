e502140ad2b422932099c8329e0eb6c7fb597562
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-03-29 16:34:00 +0900

    modified:   sec11b/Node.py

--- sec11b/Node.py
+++ sec11b/Node.py
@@ -293,7 +293,8 @@ class Node:
         # 最後に受け取ったACK番号を取得
         last_ack_number = self.tcp_connections[connection_key].get("last_ack_number")
 
-        print(f"Last ACK number: {last_ack_number}, Current ACK number: {current_ack_number}, self.tcp_connections[connection_key]["duplicate_ack_count"]")
+        n = self.tcp_connections[connection_key]["duplicate_ack_count"]
+        print(f"{self.node_id}  Last ACK number: {last_ack_number}, Current ACK number: {current_ack_number}, {n}")
         if current_ack_number == last_ack_number:
             # 重複ACKとみなしてカウントアップ
             self.tcp_connections[connection_key]["duplicate_ack_count"] += 1
