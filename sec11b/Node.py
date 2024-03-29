da0ef68d4373d10de9ac896e65449729cdcd0e80
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-03-29 15:42:45 +0900

    modified:   sec11b/Node.py

--- sec11b/Node.py
+++ sec11b/Node.py
@@ -360,6 +360,7 @@ class Node:
             if self.network_event_scheduler.tcp_verbose:
                 print(f"Updated ACK number to {next_expected_seq} for connection {connection_key}.")
 
+        print(self.node_id, " received_seq: ", received_sequence_number, " current:", current_ack_number, " new:", next_expected_seq)
         # 新しいACK番号の計算
         # 受信したパケットが連続しているかどうかを確認し、連続していれば新しいACK番号を計算
         # 連続していない（受け取っていないパケットが存在する）場合は、現在のACK番号をそのまま使用
@@ -372,7 +373,7 @@ class Node:
         #    new_ack_number = current_ack_number
 
         # ACK番号を更新
-        print(self.node_id, " received_seq: ", received_sequence_number, " current:", current_ack_number, " new:", new_ack_number)
+        #print(self.node_id, " received_seq: ", received_sequence_number, " current:", current_ack_number, " new:", new_ack_number)
         #if new_ack_number != current_ack_number:
         #    self.tcp_connections[connection_key]["acknowledgment_number"] = new_ack_number
         #    if self.network_event_scheduler.tcp_verbose:
