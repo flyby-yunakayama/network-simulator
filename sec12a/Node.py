b7bfbe86dbc1c6fdeca5d45e62c88dd40866342f
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-07-14 10:15:25 +0900

    modified:   sec12a/Node.py

--- sec12a/Node.py
+++ sec12a/Node.py
@@ -24,7 +24,8 @@ class Node:
         self.tcp_connections = {}  # 接続状態を追跡する辞書
         self.cwnd = 1  # 輻輳ウィンドウの初期値
         self.ssthresh = 16  # スロースタート閾値の初期値
-        self.MAX_CWND = 64  # 例として64パケットを最大ウィンドウサイズとする
+        self.MAX_CWND = 64  # 64パケットを最大ウィンドウサイズとする
+        self.tcp_state = {}  # 状態管理
         self.max_attempts = 3  # パケット再送の最大試行回数
         self.windows = {}  # ウィンドウ内のパケットのシーケンス番号を追跡
         self.timeout_interval = 2  # タイムアウトまでの時間(秒)
@@ -329,8 +330,15 @@ class Node:
             self.tcp_connections[connection_key]["duplicate_ack_count"] = 1
             self.tcp_connections[connection_key]["last_ack_number"] = ack_number
 
-        # 輻輳ウィンドウを調整
-        self.adjust_congestion_window(connection_key)
+        if self.tcp_connections[connection_key]["duplicate_ack_count"] >= 3:
+            # Fast retransmit
+            self.tcp_connections[connection_key]['ssthresh'] = max(self.tcp_connections[connection_key]['cwnd'] // 2, 2)
+            self.tcp_connections[connection_key]['cwnd'] = 1
+            self.transition_to_state(connection_key, 'slow_start')
+            self.retransmit_packet(connection_key, ack_number)
+        else:
+            # 輻輳ウィンドウを調整
+            self.adjust_congestion_window(connection_key)
 
         # ACK番号に一致するパケットをウィンドウから削除
         for seq, packet_info in list(self.windows[connection_key].items()):
@@ -354,16 +362,16 @@ class Node:
         ssthresh = self.tcp_connections[connection_key]['ssthresh']
 
         if state == 'slow_start':
-            # スロースタート: cwndを倍増させるが、最大値を超えないようにする
-            new_cwnd = min(cwnd * 2, self.MAX_CWND)
+            # スロースタート: cwndを1 MSSずつ増加させる
+            new_cwnd = min(cwnd + 1, self.MAX_CWND)
             self.tcp_connections[connection_key]['cwnd'] = new_cwnd
             if self.network_event_scheduler.tcp_verbose:
                 print(f"Updated cwnd to {new_cwnd} for connection {connection_key} in slow start.")
 
-            if cwnd >= ssthresh:
+            if new_cwnd >= ssthresh:
                 # ssthreshに達したら輻輳回避へ移行
                 self.transition_to_state(connection_key, 'congestion_avoidance')
-        
+
         elif state == 'congestion_avoidance':
             # 輻輳回避: cwndを線形に増加
             new_cwnd = min(cwnd + 1, self.MAX_CWND)
