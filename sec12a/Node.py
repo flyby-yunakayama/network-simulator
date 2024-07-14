832c38e3f762b1b1eb7c394570fedeae82df5a66
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-07-14 11:19:24 +0900

    modified:   sec12a/Node.py

--- sec12a/Node.py
+++ sec12a/Node.py
@@ -457,7 +457,7 @@ class Node:
 
         # 新しい接続情報を初期化
         if connection_key not in self.tcp_connections:
-            self.initialize_connection_info(connection_key=connection_key, state='SYN_RECEIVED', sequence_number=sequence_number, acknowledgment_number=acknowledgment_number, data=b'')
+            self.initialize_connection_info(connection_key=connection_key, state='SYN_RECEIVED', sequence_number=sequence_number, acknowledgment_number=acknowledgment_number, data=None)
 
         # パラメータ設定
         control_packet_kwargs = {
