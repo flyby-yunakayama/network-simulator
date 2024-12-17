df3126c02cdd22b91fd2ddf77778cb7fdb7551a5
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-12-17 20:07:28 +0900

    modified:   sec14a/NetworkEventScheduler.py

--- sec14a/NetworkEventScheduler.py
+++ sec14a/NetworkEventScheduler.py
@@ -247,8 +247,6 @@ class NetworkEventScheduler:
             if "arrival_time" in log and log["arrival_time"] is not None and log["arrival_time"] > 0:
                 data["received_packets"] += 1
                 data["received_bytes"] += log["size"]
-                # send_timeがあればそちらを使用
-                print(log["arrival_time"], log["send_time"])
                 if "send_time" in log and log["send_time"] is not None:
                     delay = log["arrival_time"] - log["send_time"]
                 else:
