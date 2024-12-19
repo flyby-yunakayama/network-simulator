0143a61f433055762ce40fd289ccc063fe68325a
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-12-19 10:03:20 +0900

    modified:   sec14a/Node.py

--- sec14a/Node.py
+++ sec14a/Node.py
@@ -130,7 +130,7 @@ class Node:
     def process_ARP_packet(self, packet):
         self.network_event_scheduler.log_packet_info(packet, "arrived", self.node_id)
         packet.set_arrived(self.network_event_scheduler.current_time)
-        
+
         if packet.header["destination_mac"] == "FF:FF:FF:FF:FF:FF":
             if packet.payload.get("operation") == "request" and packet.payload["destination_ip"] == self.ip_address:
                 self._send_arp_reply(packet)
@@ -429,6 +429,9 @@ class Node:
 
             self._send_transport_packet("TCP", destination_ip, destination_mac, data, dscp, **kwargs)
             self.windows[connection_key][sequence_number]["attempt"] += 1
+
+            # 再送パケットに対して改めてタイムアウトを設定
+            self.schedule_timeout(connection_key, sequence_number)
 
             if self.windows[connection_key][sequence_number]["attempt"] >= self.max_attempts:
                 # 最大試行回数到達時の処理
