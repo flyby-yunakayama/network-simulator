36e7a6d3668ee6ff1b694dabd23ff9f6d8d543ef
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-12-24 08:07:13 +0900

    modified:   scenario14a.py

--- scenario14a.py
+++ scenario14a.py
@@ -1,80 +1,73 @@
+import sys
+
+!git clone https://github.com/flyby-yunakayama/network-simulator.git
+sys.path.insert(0,'/content/network-simulator')
+%cd network-simulator
+
 from sec14a.NetworkEventScheduler import NetworkEventScheduler
 from sec14a.Node import Node
 from sec14a.Switch import Switch
+from sec14a.Router import Router
+from sec14a.Server import DNSServer, DHCPServer
 from sec14a.Link import Link
-from sec14a.Application import UDPApp, FTPClient, FTPServer
+from sec14a.Application import DnsClient, DhcpClient, UDPApp, FTPClient, FTPServer
 
 nes = NetworkEventScheduler(seed=7, log_enabled=True, verbose=False, tcp_verbose=True, link_verbose=False)
 
-# ノードの設定（サーバ側）
-server1 = Node(node_id="server1", ip_address="192.168.1.250/24", network_event_scheduler=nes)
-# FTPサーバインスタンス
-ftp_server = FTPServer(server1, shared_files={}, verbose=True)
-# 21/TCPポートでFTPサーバを起動（待ち受け）
-server1.register_application(21, "TCP", ftp_server)
-
-# ノードの設定（クライアント側）
-client1 = Node(node_id="client1", ip_address="192.168.1.1/24", network_event_scheduler=nes)
-# FTPクライアントインスタンス
-ftp_client = FTPClient(client1, verbose=False)
-# FTPクライアントをApplicationManagerに登録
-client1.register_application(0, "TCP", ftp_client)
-
-# UDP送信アプリケーションのインスタンス作成
-udp_app = UDPApp(client1)
+# ノードとルータの設定
+node1 = Node(node_id="n1", ip_address="192.168.1.0/24", network_event_scheduler=nes)  # DHCP利用
+node2 = Node(node_id="n2", ip_address="192.168.2.1/24", network_event_scheduler=nes)  # IPアドレスを静的に設定
+switch1 = Switch(node_id="s1", ip_address="192.168.1.240/24", network_event_scheduler=nes)
+router1 = Router(node_id="r1", ip_addresses=["192.168.1.254/24", "192.168.2.254/24"], network_event_scheduler=nes)
+dns1 = DNSServer(node_id="dns1", ip_address="192.168.1.200/24", network_event_scheduler=nes)
+dhcp1 = DHCPServer(node_id="dhcp1", ip_address="192.168.1.250/24", dns_server_ip="192.168.1.200/24", start_cidr="192.168.1.0/24", network_event_scheduler=nes)
 
 # リンクの設定
-link1 = Link(client1, server1, bandwidth=1000000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link1 = Link(node1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.1, network_event_scheduler=nes)
+link2 = Link(switch1, router1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link3 = Link(dns1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link4 = Link(dhcp1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link5 = Link(node2, router1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
 
 # ネットワークのトポロジを描画
-#nes.draw()
+nes.draw()
 
-# ファイル読み込み（サーバ側ファイル想定）
-file_path = 'data/cat.jpg'
-with open(file_path, 'rb') as f:
-    file_data = f.read()
+# DNSサーバに対してDNSレコード設定
+dns1.add_dns_record("www.example.com", "192.168.2.1/24")
 
-# サーバにファイル登録
-ftp_server.shared_files["cat.jpg"] = file_data
+# DHCPサーバに対して使用済みIPアドレス登録
+used_ips = ["192.168.1.240/24", "192.168.1.254/24", "192.168.1.200/24", "192.168.1.250/24"]
+dhcp1.mark_ips_as_used(used_ips)
 
-# クライアント側でFTP転送を開始する処理
-# ここでは1.0秒後に実行されるようスケジューリング
-def start_ftp_transfer():
-    ftp_client.connect(server_ip="192.168.1.250/24", server_port=21)
-    # コネクション確立後、USER/PASSとRETR testfile.txtを行うようなロジックをFTPClient内に実装
-    ftp_client.retrieve_file("cat.jpg")
-
-# UDPトラフィック開始処理
+# UDP送信アプリケーションのインスタンス作成
+udp_app = UDPApp(node1)
 def start_udp_traffic():
-    # 2.0秒後からUDPトラフィックを5秒間、bitrate=1Mbps, payload=72バイト、header_size=28バイトで送信
-    udp_app.start_traffic(destination_url="192.168.1.250/24",
+    udp_app.start_traffic(destination_url="www.example.com",
                           bitrate=100000,
                           start_time=2.0,
                           duration=5.0,
                           header_size=28,
-                          payload_size=72,
+                          payload_size=1000,
                           burstiness=1.0,
                           dscp=16)
 
-# 1秒後にFTP転送開始
-nes.schedule_event(1.0, start_ftp_transfer)
+nes.schedule_event(2.0, start_udp_traffic)
 
-# UDPトラフィックも2秒後から開始
-#nes.schedule_event(2.0, start_udp_traffic)
 
 # イベントスケジューラを実行
 nes.run_until(10.0)
+
+# フォワーディングテーブルやルーティングテーブルを確認
+node1.print_arp_table()
+node1.print_url_to_ip_mapping()
+node1.print_tcp_connections()
+router1.print_arp_table()
+router1.print_routing_table()
+node2.print_arp_table()
+node2.print_tcp_connections()
 
 # 結果を確認
 nes.generate_summary(nes.packet_logs)
 nes.generate_throughput_graph(nes.packet_logs)
 nes.generate_delay_histogram(nes.packet_logs)
-
-# 転送進行状況のプロット
-nes.plot_cwnd_log()
-
-ftp_connections = [ck for ck, info in server1.tcp_connections.items() if info.get('transfer_info') and info['transfer_info'].get('progress')]
-if ftp_connections:
-    # 複数あれば先頭を選択
-    connection_key = ftp_connections[0]
-    nes.plot_transfer_progress(server1, connection_key)
+#nes.plot_cwnd_log()
