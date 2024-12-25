11c1916604eeafb97cca75d1bb58738523713ce4
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-12-25 10:31:11 +0900

    new file:   scenario15a.py

---
+++ scenario15a.py
@@ -0,0 +1,78 @@
+import sys
+
+!git clone https://github.com/flyby-yunakayama/network-simulator.git
+sys.path.insert(0,'/content/network-simulator')
+%cd network-simulator
+
+from sec15a.NetworkEventScheduler import NetworkEventScheduler
+from sec15a.Node import Node
+from sec15a.Switch import Switch
+from sec15a.Router import Router
+from sec15a.Server import DNSServer, DHCPServer
+from sec15a.Link import Link
+from sec15a.Application import DnsClient, DhcpClient, UDPApp, FTPClient, FTPServer, HTTPSClient, HTTPSServer
+
+nes = NetworkEventScheduler(seed=7, log_enabled=True, verbose=False, tcp_verbose=True, link_verbose=False)
+
+# ノードとルータの設定
+node1 = Node(node_id="n1", ip_address="192.168.1.0/24", network_event_scheduler=nes)  # DHCP利用
+node2 = Node(node_id="n2", ip_address="192.168.2.1/24", network_event_scheduler=nes)  # IPアドレスを静的に設定
+switch1 = Switch(node_id="s1", ip_address="192.168.1.240/24", network_event_scheduler=nes)
+router1 = Router(node_id="r1", ip_addresses=["192.168.1.254/24", "192.168.2.254/24"], network_event_scheduler=nes)
+dns1 = DNSServer(node_id="dns1", ip_address="192.168.1.200/24", network_event_scheduler=nes)
+dhcp1 = DHCPServer(node_id="dhcp1", ip_address="192.168.1.250/24", dns_server_ip="192.168.1.200/24", start_cidr="192.168.1.0/24", network_event_scheduler=nes)
+
+# リンクの設定
+link1 = Link(node1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link2 = Link(switch1, router1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link3 = Link(dns1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link4 = Link(dhcp1, switch1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+link5 = Link(node2, router1, bandwidth=100000, delay=0.01, loss_rate=0.0, network_event_scheduler=nes)
+
+# ネットワークのトポロジを描画
+nes.draw()
+
+# DNSサーバに対してDNSレコード設定
+dns1.add_dns_record("www.example.com", "192.168.2.1/24")
+
+# DHCPサーバに対して使用済みIPアドレス登録
+used_ips = ["192.168.1.240/24", "192.168.1.254/24", "192.168.1.200/24", "192.168.1.250/24"]
+dhcp1.mark_ips_as_used(used_ips)
+
+# HTTPSサーバの設定 (ポート443)
+shared_files = {
+    "index.html": b"<h1>Welcome to Secure example.com!</h1>",
+    "secret.txt": b"TopSecretData",
+    "large.dat": b"X" * 10000  # 10KB file for testing
+}
+https_server = HTTPSServer(node2, shared_files=shared_files, verbose=True)
+node2.register_application(443, "TCP", https_server)  # HTTPSサーバをApplicationManagerに登録
+
+# HTTPSクライアントの設定
+https_client = HTTPSClient(node1, verbose=True)
+node1.register_application(0, "TCP", https_client)  # HTTPSクライアントをApplicationManagerに登録
+
+def start_https_download():
+    https_client.connect(server_url="www.example.com", server_port=443)
+    https_client.get_file("index.html")
+
+# HTTPSダウンロードを2秒後に開始
+nes.schedule_event(2.0, start_https_download)
+
+# イベントスケジューラを実行
+nes.run_until(10.0)
+
+# フォワーディングテーブルやルーティングテーブルを確認
+node1.print_arp_table()
+node1.print_url_to_ip_mapping()
+node1.print_tcp_connections()
+router1.print_arp_table()
+router1.print_routing_table()
+node2.print_arp_table()
+node2.print_tcp_connections()
+
+# 結果を確認
+nes.generate_summary(nes.packet_logs)
+nes.generate_throughput_graph(nes.packet_logs)
+nes.generate_delay_histogram(nes.packet_logs)
+#nes.plot_cwnd_log()
