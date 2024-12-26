ca8bfaec8289cba7861f999293354505e4fb682f
Yu Nakayama <yu.nakayama@flyby.co.jp>
2024-12-26 09:28:47 +0900

    modified:   sec15a/Application.py

--- sec15a/Application.py
+++ sec15a/Application.py
@@ -1060,8 +1060,6 @@ class TLSServer:
         HTTPSサーバ(=HTTPServer継承)から呼ばれ、TLSハンドシェイク中のメッセージかどうかを判別する。
         """
         data = packet.payload
-        print(packet)
-        print(packet.payload)
         if not data:
             # **追加：空ペイロードはACK等とみなし無視する**
             if self.verbose:
